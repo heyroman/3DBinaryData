2c4a49c78269077dde9ea5644f9ac264d2d33e0e
Roman Preobrazhensky <cogtepsum@gmail.com>
2015-08-19 16:24:58 +0300

    more decent binary difference calculation and comments

--- code/countMatchingBits.py
+++ code/countMatchingBits.py
@@ -1,9 +1,20 @@
+# This function just counts the number of matching bits amongst passed bytes
+# If bytes has different length in binary representation,
+# than the number of compared bits is equal to a length of the shortest byte
+
+
 def count_matching_bits(*data):
+    maxlen = len(bin(max(data))[2:])
+    data = [bin(e)[2:] for e in data]
+    for i in range(len(data)):
+        while len(data[i]) < maxlen:
+            data[i] = '0' + data[i]
+
     result = 0
-    for i in range(bin(min(data)).__len__()):
+    for i in range(len(data[0])):   # take an index for a bit
         is_similar = True
-        for j in range(data.__len__()):
-            is_similar *= (bin(data[0])[i] == bin(data[j])[i])
+        for j in range(len(data)):  # iterate over bytes in data
+            is_similar *= (data[0][i] == data[j][i])
         if is_similar:
             result += 1
     return result
--- code/visualisation.py
+++ code/visualisation.py
@@ -7,44 +7,56 @@ import matplotlib.cm as cmx
 import matplotlib.colors
 from countMatchingBits import *
 
+# initializing variable that will store our data
 dataX = []
 dataY = []
 dataZ = []
 dataC = []
 
-
-# filename = input("Enter file name: ")
-filename = 'comsol_chart.txt'
+# reading a file from 'input_data_examples' directory
+filename = input("Enter file name: ")
+# filename = 'comsol_chart.txt'
 pathname = os.path.join('..', os.path.join('input_data_examples', filename))
 file = open(pathname, 'rb')
+
 try:
+    # reading 3 bytes..
     byteX = file.read(1)
     byteY = file.read(1)
     byteZ = file.read(1)
+
+    # while bytes are not empty do processing and reading
     while (byteX.__len__() == 1) & (byteY.__len__() == 1) & (byteZ.__len__() == 1):
+        # unpacking read bytes into decimal integers (cartesian coordinates)
         byteX = unpack('B', byteX)[0]
         byteY = unpack('B', byteY)[0]
         byteZ = unpack('B', byteZ)[0]
 
+        # adding these integers to our storages
         dataX.append(byteX)
         dataY.append(byteY)
         dataZ.append(byteZ)
+        # and the result of function of these integers as well
         dataC.append(count_matching_bits(byteX, byteY, byteZ))
 
+        #read next 3 bytes
         byteX = file.read(1)
         byteY = file.read(1)
         byteZ = file.read(1)
 finally:
     file.close()
+
 dataX = random.sample(dataX, 1500)
 dataY = random.sample(dataY, 1500)
 dataZ = random.sample(dataZ, 1500)
 dataC = random.sample(dataC, 1500)
 
+# creating colormap according to present data
 cm = plt.get_cmap('brg')
 cNorm = matplotlib.colors.Normalize(vmin=min(dataC), vmax=max(dataC))
 scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
 
+# plotting
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d', axisbg='k')
 ax.w_xaxis.set_pane_color((0, 0, 0))
