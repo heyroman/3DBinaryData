be4af2035ff4baca5fd5c2da15eb19126609746a
Roman Preobrazhensky <cogtepsum@gmail.com>
2015-08-19 19:16:42 +0300

    plot improvement and images import for gif creationship

--- code/visualisation.py
+++ code/visualisation.py
@@ -14,8 +14,9 @@ dataZ = []
 dataC = []
 
 # reading a file from 'input_data_examples' directory
-filename = input("Enter file name: ")
-# filename = 'comsol_chart.txt'
+
+# filename = input("Enter file name: ")
+filename = 'java_jar.jar'
 pathname = os.path.join('..', os.path.join('input_data_examples', filename))
 file = open(pathname, 'rb')
 
@@ -58,12 +59,30 @@ scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
 
 # plotting
 fig = plt.figure()
+fig.set_facecolor('black')
 ax = fig.add_subplot(111, projection='3d', axisbg='k')
+ax.axis('off')
+# ax.patch.set_visible(False)
 ax.w_xaxis.set_pane_color((0, 0, 0))
 ax.w_yaxis.set_pane_color((0, 0, 0))
 ax.w_zaxis.set_pane_color((0, 0, 0))
 ax.grid(False)
-ax.scatter(dataX, dataY, dataZ, c=scalarMap.to_rgba(dataC), edgecolors=scalarMap.to_rgba(dataC), marker='.', s=1)
+ax.set_xlim([-50, 300])
+ax.set_ylim([-50, 300])
+ax.set_zlim([-50, 300])
+ax.scatter(dataX, dataY, dataZ, c=scalarMap.to_rgba(dataC), edgecolors=scalarMap.to_rgba(dataC), marker='.', s=5)
 scalarMap.set_array(dataC)
-fig.colorbar(scalarMap)
-plt.show()
+plt.subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0)
+# plt.show()
+
+# creating a bunch of *png for gif
+
+for n in range(0, 100):
+    ax.azim = ax.azim + 3.6
+    # if n >= 74 and n <= 76:
+    #     ax.elev = ax.elev-0.2
+    #     ax.azim = ax.azim+0.5 #end by panning/rotating slowly to stopping position
+    plt.savefig('../images/images_for_gif/img' + str(n).zfill(3) + '.png',
+                bbox_inches='tight')
+    print(n)
+plt.close()
